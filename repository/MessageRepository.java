1ace4016d48db7ebc89a20493005f8a14255f64e
Greenlyte712 <91234693+Greenlyte712@users.noreply.github.com>
2025-03-28 14:35:12 -0400

    Delete repository directory

--- repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greenlyte712.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greenlyte712.model.message.Message;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-    // Custom query methods can be defined here if needed
-}
